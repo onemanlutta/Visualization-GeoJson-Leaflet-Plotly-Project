dfc5117feb643d8e3485599ee767c4651d90fb16
NicholasJWiid <nicholasjameswiid@gmail.com>
2024-06-21 16:16:23 -0700

    backup of progress - drop down text complete

--- js/dropdown_logic.js
+++ js/dropdown_logic.js
@@ -1,91 +1,125 @@
 // Define a new array variable to hold the data
-let meteoriteData = []
-var myMap
+var meteoriteData = []
+var newMeteoritesLayer;
 
 // Define function to fetch data from local server
 
-d3.selectAll('#dropdownMenu').on('change', fetchData)
+d3.selectAll('#dropdownMenu').on('change', fetchDataAndSetGlobal)
 
+// Function to perform asynchronous operation and return a Promise
 function fetchData() {
-    d3.json('http://localhost:3000/api/data').then(data => {
-        meteoriteData = data;
-
-
-        let dropdownMenu = d3.select('#dropdownMenu')
-        var yearGroup = dropdownMenu.property("value");
-        let yearData = []
-
-        function yearSelect(meteoriteData) {
-            if (yearGroup === 'default') {
-                yearData = meteoriteData
-                console.log('default')
-            }
-            else if (yearGroup === '0-1950') {
-                yearData = meteoriteData.filter(data => data.year_bin == '1950 & Prior')
-                console.log('0')
-            }
-            else if (yearGroup === '1950-1959') {
-                console.log('1')
-            }
-            else if (yearGroup === '1960-1969') {
-                console.log('2')
-            }
-            else if (yearGroup === '1970-1979') {
-                console.log('3')
-            }
-            else if (yearGroup === '1980-1989') {
-                console.log('4')
-            }
-            else if (yearGroup === '1990-1999') {
-                console.log('5')
-            }
-            else if (yearGroup === '2000-2009') {
-                console.log('6')
-            }
-            else if (yearGroup === '2010-2019') {
-                console.log('7')
-            }
-            else if (yearGroup === '2020 to date') {
-                console.log('8')
-            }
-            else if (yearGroup === '65 million-') {
-                console.log('9')
-            }
-            else {
-                console.log('10')
-            }
-            return yearData
-        }
-        yearData = yearSelect(meteoriteData)
-
-
-        // Convert data to geoJSON
-        const geojsonData = {
-            type: "FeatureCollection",
-            features: yearData.map(meteorite => ({
-                type: "Feature",
-                properties: {
-                    name: meteorite.name,
-                    nametype: meteorite.nametype,
-                    recclass: meteorite.recclass,
-                    mass: meteorite.mass,
-                    fall: meteorite.fall,
-                    year: meteorite.year,
-                    reclat: meteorite.reclat,
-                    reclong: meteorite.reclong,
-                    class_name: meteorite.class_name,
-                    group_name: meteorite.group_name,
-                    year_bin: meteorite.year_bin,
-                    log_mass: meteorite.log_mass,
-                },
-                geometry: meteorite.geometry
-            }))
-        };
-        console.log(geojsonData)
-        // Run the create features function with converted data
-        createFeatures(geojsonData)
-    })
+  return new Promise((resolve, reject) => {
+    fetch('http://localhost:3000/api/data').then(response => {
+        return response.json();
+      }).then(data => {
+        resolve(data);
+      });
+  });
 }
+
+function makeLayers(meteoriteData) {
+    console.log(meteoriteData)
+    let dropdownMenu = d3.select('#dropdownMenu')
+    var yearGroup = dropdownMenu.property("value");
+    let yearData = []
+
+    function yearSelect(meteoriteData) {
+        if (yearGroup === 'default') {
+            yearData = meteoriteData
+            console.log('default')
+        }
+        else if (yearGroup === '0-1950') {
+            yearData = meteoriteData.filter(data => data.year_bin == '1950 & Prior')
+            console.log('0')
+        }
+        else if (yearGroup === '1950-1959') {
+            yearData = meteoriteData.filter(data => data.year_bin == '1950-1959')
+            console.log('1')
+        }
+        else if (yearGroup === '1960-1969') {
+            yearData = meteoriteData.filter(data => data.year_bin == '1960-1969')
+            console.log('2')
+        }
+        else if (yearGroup === '1970-1979') {
+            yearData = meteoriteData.filter(data => data.year_bin == '1970-1979')
+            console.log('3')
+        }
+        else if (yearGroup === '1980-1989') {
+            yearData = meteoriteData.filter(data => data.year_bin == '1980-1989')
+            console.log('4')
+        }
+        else if (yearGroup === '1990-1999') {
+            yearData = meteoriteData.filter(data => data.year_bin == '1990-1999')
+            console.log('5')
+        }
+        else if (yearGroup === '2000-2009') {
+            yearData = meteoriteData.filter(data => data.year_bin == '2000-2009')
+            console.log('6')
+        }
+        else if (yearGroup === '2010-2019') {
+            yearData = meteoriteData.filter(data => data.year_bin == '2010-2019')
+            console.log('7')
+        }
+        else if (yearGroup === '2020 to date') {
+            yearData = meteoriteData.filter(data => data.year_bin == '2020 to date')
+            console.log('8')
+        }
+        else if (yearGroup === '65 million-') {
+            yearData = meteoriteData.filter(data => data.year_bin == '65 million years ago')
+            console.log('9')
+        }
+        return yearData
+    }
+    yearData = yearSelect(meteoriteData)
+
+
+    // Convert data to geoJSON
+    const geojsonData = {
+        type: "FeatureCollection",
+        features: yearData.map(meteorite => ({
+            type: "Feature",
+            properties: {
+                name: meteorite.name,
+                nametype: meteorite.nametype,
+                recclass: meteorite.recclass,
+                mass: meteorite.mass,
+                fall: meteorite.fall,
+                year: meteorite.year,
+                reclat: meteorite.reclat,
+                reclong: meteorite.reclong,
+                class_name: meteorite.class_name,
+                group_name: meteorite.group_name,
+                year_bin: meteorite.year_bin,
+                log_mass: meteorite.log_mass,
+                meteorite_width_cm: meteorite.meteorite_width_cm
+            },
+            geometry: meteorite.geometry
+        }))
+    };
+    console.log(geojsonData)
+    // Run the create features function with converted data
+  
+    createFeatures(geojsonData)
+}
+
+
+// Function to initiate fetching data and set global variable after a timeout
+function fetchDataAndSetGlobal() {
+  fetchData().then(data => {
+    // Store data in global variable after 3 seconds
+    setTimeout(() => {
+      meteoriteData = data;
+      console.log("Global variable 'meteoriteData' has been set:", meteoriteData);
+      
+      makeLayers(meteoriteData)
+    }, 3000); // Set globalData after 3 seconds
+  
+}).catch(error => {
+    console.error('Error fetching data:', error);
+
+  });
+}
+
 
 // Define function to choose circle marker colors
 function colorpicker(meteorite_type) {
@@ -98,13 +132,18 @@ function colorpicker(meteorite_type) {
 
 // Define function to create the map features
 function createFeatures(meteorite_data) {
-
-    // Add popup layer
+    // Remove the existing meteoritesLayer if it exists
+  if (newMeteoritesLayer) {
+    myMap.removeLayer(newMeteoritesLayer);
+    layerControl.removeLayer(newMeteoritesLayer);
+  }
+   
+   
     function onEachFeature(feature, layer) {
         layer.bindPopup(`<h3>Meteorite Name: ${feature.properties.name}</h3> <h3>Meteritie Rock-type: ${feature.properties.group_name}</h3> <h3>Meteorite Class: ${feature.properties.class_name}</h3>`)
 }
 // Add circle marker layer
-let meteoritesLayer = L.geoJson(meteorite_data, {
+newMeteoritesLayer = L.geoJson(meteorite_data, {
 
 
 onEachFeature: onEachFeature,
@@ -120,15 +159,13 @@ pointToLayer: function (feature, latlng) {
   }
 })
 
-// Create the map using feature variables created
-// if (myMap) {
-//   myMap.remove()
-  createMap(meteoritesLayer)
-  // }
+// Add overlay maps overlay maps
+myMap.addLayer(newMeteoritesLayer)
+layerControl.addOverlay(newMeteoritesLayer, "Meteors")
+
 }
 
-// Define the function to crete the map
-function createMap(meteoritesLayer) {
+
 // Add the tile layer smooth
 var smooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
 minZoom: 0,
@@ -141,10 +178,8 @@ ext: 'png'
 let baseMaps = {
 Basic: smooth
 };
-// Define overlay maps
-let overlayMaps = {
-"Meteorite locations": meteoritesLayer
-};
+
+let overlayMaps = {}
 
 // Create the map object
 let myMap = L.map("map", {
@@ -152,17 +187,16 @@ center: [0, 0],
 zoom: 2,
 minZoom: 3,
 // maxZoom: 3,
-layers: [smooth, meteoritesLayer]
+layers: [smooth]
 });
 
 // Add control layers
-L.control.layers(baseMaps, overlayMaps, {
-collapsed: false
-}).addTo(myMap);
-};
+var layerControl = L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+    }).addTo(myMap);
 
 // Fetch data from local sever an run maps
-fetchData()
+fetchDataAndSetGlobal()
 
 
 ///// Dropdown setup
